fce181f4de5eddf2c34c74929839822b238ddcc1
PaperAnchor <zach@paperanchor.ca>
2016-03-10 16:35:58 -0700

    fixed homepage styling and added dropdown menu

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,8 +1,8 @@
 class BusinessesController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show, :search]
-  
-  
+
+
   def search
     if params[:search].present?
       @businesses = Business.search(params[:search])
@@ -10,32 +10,32 @@ class BusinessesController < ApplicationController
       @businesses = Business.all.paginate(page: params[:page], per_page: 6)
     end
   end
-  
+
   def index
     @businesses = Business.all.paginate(page: params[:page], per_page: 6)
   end
-  
+
   def new
     @business = Business.new
   end
-  
+
   def create
     @business = Business.new post_params
-    
+
     if @business.save
-      redirect_to @business, notice: "Hell Ya! You posted a new business!"
+      redirect_to @business, notice: "A new business has been added successfully!"
     else
-      render 'new', notice: "Oh no! That didn't work!"
+      render 'new', notice: "Oops. Something went wrong. Try again later."
     end
   end
-  
+
   def show
     @businesses = Business.all
   end
-  
+
   def edit
   end
-  
+
   def update
     if @business.update post_params
       redirect_to @business, notice: "Yay! The business info has been updated!"
@@ -43,18 +43,18 @@ class BusinessesController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def destroy
     @business.destroy
     redirect_to businesses_path
   end
-  
+
   private
-  
+
   def post_params
     params.require(:business).permit(:name, :address, :city, :postal, :email, :phone, :url, :description, :keywords, :logo, :audio, :slug, :category_id)
   end
-  
+
   def find_post
     @business = Business.friendly.find(params[:id])
   end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
-  end 
+    @businesses = Business.all.paginate(page: params[:page], per_page: 6)
+  end
+
+  def show
+    @businesses = Business.all
+  end
 end
